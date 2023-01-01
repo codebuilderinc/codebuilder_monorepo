a2db4bbf6955b3caff966d944e601b3bcf0c1c75
Andrew Corbin <andrew.c.corbin@gmail.com>
2022-12-31 23:44:24 -0600

    Scheduled reddit scrape.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('scrapeReddit')->hourly();
+        $schedule->command('scrapeReddit')->everyMinute();
     }
 
     /**
